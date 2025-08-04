bc5b855c5ae0bdc0546eb9fd14d79ae961bfe365
Amir Hosein Sedaghati <amirhosseinsedaghati42@gmail.com>
2025-08-03 23:39:13 -0400

    Add export_schema_into_a_binary_file.py

---
+++ export_schema_into_a_binary_file.py
@@ -0,0 +1,24 @@
+import pyodbc
+import subprocess
+
+
+# Connect to SQL Server
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 18 for SQL Server};"
+    "SERVER=localhost;"
+    "DATABASE=Amazon;"
+    "UID=SA;"
+    "PWD=sixteen240CR7;"
+    "TrustServerCertificate=yes;" # to bypass SSL verification
+)
+
+# Get diagram binary
+cursor = conn.cursor()
+cursor.execute("SELECT definition FROM dbo.sysdiagrams WHERE name = 'Amazon_Diagram'")
+diagram_data = cursor.fetchone()[0]
+
+# Save binary file
+with open('Amazon_Diagram.bin', 'wb') as f:
+    f.write(diagram_data)
+
+print("Diagram binary saved as Amazon_Diagram.bin")
